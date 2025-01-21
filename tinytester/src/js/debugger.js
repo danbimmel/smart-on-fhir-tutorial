9a669c62ec21b56d7fdb7c5edb1dfb9a2475638e
danbimmel <daniel@bimmel-consulting.com>
2025-01-21 23:21:37 +0100

    test9

--- tinytester/src/js/debugger.js
+++ tinytester/src/js/debugger.js
@@ -16,3 +16,17 @@ http.createServer(function(req, res){
 // fake worker of some kind
 
 require('./worker');
+
+// require the core node events module
+var EventEmitter = require('events').EventEmitter
+
+//create a new event emitter
+var emitter = new EventEmitter
+
+// set up a listener for the event
+emitter.on('pizza', function (message) {
+  console.log(message)
+})
+
+// emit an event
+emitter.emit('pizza', 'pizza is extremely yummy')
